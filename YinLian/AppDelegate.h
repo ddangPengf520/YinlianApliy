3295f361df47a23f116e6f1528dbc306f798fa9d
dpfst520 <dpfst520@imac.local>
2015-12-01 09:54:35 +0800

    Initial Commit

---
+++ YinLian/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  YinLian
+//
+//  Created by dpfst520 on 15/12/1.
+//  Copyright © 2015年 pengfei.dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ YinLian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YinLian
+//
+//  Created by dpfst520 on 15/12/1.
+//  Copyright © 2015年 pengfei.dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
